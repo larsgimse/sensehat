3d5f3686a2af320706e10025251dcbbb04b421fe
Lars Gimse <lagi@iktsenteret.no>
2016-08-17 13:22:12 +0200

    Add files via upload

---
+++ pixle_live.py
@@ -0,0 +1,41 @@
+from sense_hat import SenseHat
+from time import sleep
+
+sense = SenseHat()
+
+s = [0, 0, 0]  # Svart
+h = [255, 255, 255]  # Hvit
+r = [255, 0, 0] # Rød
+y = [255, 255,0] # Gul (Yellow)
+g = [0, 255, 0] # Grønn
+b = [0, 0, 255] # Blå
+
+# Bytt ut H (Hvit) med ønsket farge for å lage figur
+
+pixle1 = [
+h,h,h,h,h,h,h,h,
+h,r,r,h,h,r,r,h,
+h,r,g,h,h,r,g,h,
+h,h,h,h,h,h,h,h,
+h,h,h,r,r,h,h,h,
+h,r,h,h,h,h,r,h,
+h,h,r,r,r,r,h,h,
+h,h,h,h,h,h,h,h
+  ]
+  
+pixle2 = [
+h,h,h,h,h,h,h,h,
+h,r,r,h,h,r,r,h,
+h,g,r,h,h,g,r,h,
+h,h,h,h,h,h,h,h,
+h,h,h,r,r,h,h,h,
+h,h,h,h,h,h,h,h,
+h,r,r,r,r,r,r,h,
+h,h,h,h,h,h,h,h
+  ]
+
+while True: #løkke for å bytte mellom pixle1 og pixle2
+  sense.set_pixels(pixle1)
+  sleep(1)
+  sense.set_pixels(pixle2)
+  sleep(1)
