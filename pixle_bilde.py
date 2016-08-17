d9973ba1807ad639a1b756b6da8b6eef6c1ee4a3
Lars Gimse <lagi@iktsenteret.no>
2016-08-17 14:26:19 +0200

    Update pixle_bilde.py

--- pixle_bilde.py
+++ pixle_bilde.py
@@ -4,12 +4,12 @@ sense = SenseHat()
 
 S = [0, 0, 0]  # Svart
 H = [255, 255, 255]  # Hvit
-R = [255, 0, 0] # Rød
+R = [255, 0, 0] # Roed
 Y = [255, 255,0] # Gul (Yellow)
-G = [0, 255, 0] # Grønn
-B = [0, 0, 255] # Blå
+G = [0, 255, 0] # Groenn
+B = [0, 0, 255] # Blaa
 
-# Bytt ut H (Hvit) med ønsket farge for å lage figur
+# Bytt ut H (Hvit) med oensket farge for aa lage figur
 
 pixle = [
 H, H, H, H, H, H, H, H,
