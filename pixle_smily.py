308fce719f9ddf8c697af74bbfbb7595ed327ff2
Lars Gimse <lagi@iktsenteret.no>
2016-08-17 13:19:16 +0200

    Add files via upload

---
+++ pixle_smily.py
@@ -0,0 +1,12 @@
+Python 3.5.1 (v3.5.1:37a07cee5969, Dec  5 2015, 21:12:44) 
+[GCC 4.2.1 (Apple Inc. build 5666) (dot 3)] on darwin
+Type "copyright", "credits" or "license()" for more information.
+>>> WARNING: The version of Tcl/Tk (8.5.9) in use may be unstable.
+Visit http://www.python.org/download/mac/tcltk/ for current information.
+
+================ RESTART: /Users/lagi/Desktop/pixle_smiley.py ================
+Traceback (most recent call last):
+  File "/Users/lagi/Desktop/pixle_smiley.py", line 1, in <module>
+    from sense_hat import SenseHat
+ImportError: No module named 'sense_hat'
+>>> 
